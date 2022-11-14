99d37f57f18d0c6253c235cb07b341ff43d3114b
Rusaykin Egor <Egor.Rusaykin@swtecnn.com>
2022-11-14 12:34:13 +0300

    Create a project structure

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Login_page():
+    def __init__(self, driver):
+        self.driver = driver
+
+    def authorization(self, login_name, login_password):
+        user_name = WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='user-name']")))  # data test Xpath
+        user_name.send_keys(login_name)
+
+        password = WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id='password']")))  # password CSS selector
+        password.send_keys(login_password)
+
+        button_login = WebDriverWait(self.driver, 30).until(
+            EC.element_to_be_clickable((By.XPATH, "//input[@id ='login-button']")))
+        button_login.click()
