4047b2d6ee6a870fe9dbf2d94b94d293f91e9f59
Rusaykin Egor <Egor.Rusaykin@swtecnn.com>
2022-11-15 15:35:33 +0300

    Add two methods: Get current url and assert word

--- base/base_class.py
+++ base/base_class.py
@@ -2,3 +2,20 @@
 class Base():
     def __init__(self, driver):
         self.driver = driver
+
+    """Method get current url"""
+    def get_current_url(self):
+        get_url = self.driver.current_url
+        print("current url " + get_url)
+
+
+    """Method assert word"""
+
+    def assert_word(self, word, result):
+
+# text_products = driver.find_element(By.XPATH, "//*[@class='title']")
+# value_text_products = text_products.text
+# assert value_text_products == "PRODUCTS"
+# print("Good")
+        value_word = word.text
+        assert value_word == result
--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 
 from pages.login_page import Login_page
 
