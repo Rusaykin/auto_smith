f217101ebf00aa374a4a27a34039924308e2863b
Rusaykin Egor <Egor.Rusaykin@swtecnn.com>
2022-11-17 13:55:14 +0300

    Add Main Page and add new method - Add product_1 to the cart

--- tests/test_buy_product.py
+++ tests/test_buy_product.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
 from pages.login_page import Login_page
+from pages.main_page import Main_page
 
 
 def test_buy_product():
@@ -16,8 +17,7 @@ def test_buy_product():
     login = Login_page(driver)
     login.authorization()
 
-    enter_shopping_cart = WebDriverWait(driver, 30).until(
-        EC.element_to_be_clickable((By.XPATH, "//div[@id='shopping_cart_container']")))
-    enter_shopping_cart.click()
+    mp = Main_page(driver)
+    mp.select_product()
 
     time.sleep(5)
